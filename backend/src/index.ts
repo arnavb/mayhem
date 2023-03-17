14dadbe89b005a7695dbdf4cf474bdc107946630
Arnav Borborah <arnavborborah11@gmail.com>
2023-03-17 19:15:20 -0400

    Create basic server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,14 @@ import fastify from "fastify";
 
 const server = fastify();
 
-server.get("/ping", async (request, reply) => {
+server.get("/ping", async () => {
   return "pong";
+});
+
+server.listen({ port: 8080 }, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Server listening at ${address}`);
 });
