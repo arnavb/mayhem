8aaed5a056680b44eb3f2f6f08844564bdef1f86
Arnav Borborah <arnavborborah11@gmail.com>
2023-05-28 02:32:21 -0400

    Move away from src directory

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {},
   },
